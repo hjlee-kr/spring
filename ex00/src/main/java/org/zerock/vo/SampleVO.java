4339ee37118c53e64a6cb740fcab8a4cd5e24e79
hjlee-kr <sandjtech@naver.com>
2024-10-11 16:52:21 +0900

    20241011-Final-RestController, JS 변수

--- ex00/src/main/java/org/zerock/vo/SampleVO.java
+++ ex00/src/main/java/org/zerock/vo/SampleVO.java
@@ -1,8 +1,14 @@
 package org.zerock.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+// 모든 변수를 사용한 생성자를 만드는 어노테이션(lombok)
+@AllArgsConstructor
+// 기본생성자를 만드는 어노테이션
+@NoArgsConstructor
 public class SampleVO {
 	
 	private String name;
