7f99986f025ff80d9377597ad10038be0e75f484
hjlee-kr <sandjtech@naver.com>
2024-10-21 12:06:59 +0900

    20241021-3교시

---
+++ ex00/src/main/java/org/zerock/category/service/CategoryServiceImpl.java
@@ -0,0 +1,33 @@
+package org.zerock.category.service;
+
+import java.util.List;
+
+import org.zerock.category.vo.CategoryVO;
+
+public class CategoryServiceImpl implements CategoryService {
+
+	@Override
+	public List<CategoryVO> list(Integer cate_code1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer write(CategoryVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer update(CategoryVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer delete(CategoryVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
